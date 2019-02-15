755a29d6ca0ae1c8542a02929212811b44589bf2
gustawx <skustosz@gmail.com>
2019-02-15 18:05:38 +0100

    manual moved

---
+++ basics/functions/functions-101.js
@@ -0,0 +1,14 @@
+let square = (num = 2, bu) => {
+    let result = num*num
+    return result + "   " + bu
+}
+let nana = "fr"
+console.log(square(1,`$${nana}`))
+
+let myBook = {
+    title: '1984',
+    author: 'orwell'
+}
+
+myBook.author
+myBook.title
