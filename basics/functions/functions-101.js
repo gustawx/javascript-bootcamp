e3e73725b9f218fb4dbbd80f240ab16ad2fe26ef
Seb <skustosz@gmail.com>
2019-02-16 20:00:34 +0100

    function added

--- basics/functions/functions-101.js
+++ basics/functions/functions-101.js
@@ -7,8 +7,42 @@ console.log(square(1,`$${nana}`))
 
 let myBook = {
     title: '1984',
-    author: 'orwell'
+    author: 'orwell',
+    pageCount: 666
 }
 
-myBook.author
-myBook.title
+let getSummary = (book) => {
+    return {
+        summary: `${book.title} by ${book.author}`,
+        pageCountSummary: `${book.title} is ${book.pageCount}`
+    }
+}
+
+let myBookSummary = getSummary(myBook)
+
+console.log(`${myBookSummary.summary} === ${myBookSummary.pageCountSummary}`)
+
+//name age location
+//andrew is 27 and lives in Bubu
+//increase age by 1
+
+let andrew = {
+    age: 27,
+    name: 'Andrew',
+    location: 'Bubu'
+}
+
+console.log(`${andrew.name} is ${andrew.age} and lives in ${andrew.location}`)
+
+let increaseAge = (person) => {
+    return {
+        age: person.age + 1,
+        name: person.name,
+        location: person.location,
+    }
+}
+
+//andrew = increaseAge(andrew)
+andrew.age = andrew.age + 1
+
+console.log(`${andrew.name} is ${andrew.age} and lives in ${andrew.location}`)
